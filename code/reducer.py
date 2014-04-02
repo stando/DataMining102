ae4ed9e85f7773f4cc6b6e4b1380f44755d23089
Raynald Chung <raynaldchung@gmail.com>
2014-04-02 17:11:52 +0200

    first edition

--- code/reducer.py
+++ code/reducer.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python2.7
 
+import sys
+import numpy as np
+
+w = []
+
 if __name__ == "__main__":
-    pass
+    for line in sys.stdin:
+        line = line.strip()
+        x = np.fromstring(line,sep=" ")
+        w.append(x)
+    wei = w.mean(0)
+    for i in range(0,400,1):
+        s = 0
+        for j in range(0,10,1):
+            s = s + wei[j][i]
+        s /= 10
+        print "%f\n" % s
+
+
+
